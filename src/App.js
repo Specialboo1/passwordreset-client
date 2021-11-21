6c8b589918ded751b78798eb84e3640ee6b3e950
Specialboo1 <boopalan1314@gmail.com>
2021-11-21 21:33:21 +0530

    passwordreset-client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from "./components/signin";
+import SignUp from "./components/signup";
+import ForgetPassword from './components/forgetpassword';
+import PasswordReset from './components/passwordreset'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>      
+      <Routes>   
+          <Route path="/" element={<SignIn/>} exact/>  
+          <Route path="/signup" element={<SignUp/>} exact/>
+          <Route path="/forgetpassword" element={<ForgetPassword/>} exact/> 
+          <Route path="/passwordreset/:userId/:token" element={<PasswordReset/>} exact/>         
+      </Routes>
+    </Router> 
   );
 }
 
---
+++ src/components/forgetpassword.jsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import axios from 'axios';
+
+
+export default function ForgetPassword() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const response = await axios.post("https://boopalanpasswordreset.herokuapp.com/forgetpassword", {
+      email: data.get('email')
+    })
+    if (response.data.message !== false)
+    {
+      window.alert("Check Your Mail")
+    }
+    else
+    {
+      window.alert("User Dont Exist")      
+    }
+  };
+
+  return (
+    
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }} style={{marginTop: 10}}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Account Verification
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+           
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Submit
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+  );
+}
